2df7f8c3991d63d73db1ca411af03ecd0e75c1bb
May <mayerin1@gmail.com>
2018-11-12 13:55:24 -0500

    removing old file

--- igcm_func.py
+++ igcm_func.py
@@ -18,6 +18,8 @@ fontb.set_weight('bold')
 params = {'font.family': 'serif',}
 matplotlib.rcParams.update(params)
 
+from load_data import load_data
+
 def ortholine(R,lonl,latl,loncenter,latcenter):
     x=R*np.cos(latl)*np.sin(lonl-loncenter)
     y=R*(np.cos(latcenter)*np.sin(latl)-np.sin(latcenter)*np.cos(latl)*np.cos(lonl-loncenter))
@@ -645,3 +647,152 @@ def lon_avg(plot,path,data,lon_arr,lat_arr,lev,lo,ln,tn,noy):
 
     plt.show()
     return
+
+def lon_avg_comp(path,plot,lev,lo,ln,tn):
+    nnames=0
+    print '********** Maximum of 4 RUNS currently **********'
+    print '*************************************************'
+    ###################
+    enter='y'#raw_input('Enter another run name?? (y or n)')
+    if 'y' in enter:
+        runname_1,lon_arr_1,lat_arr_1,oom_1,surfp_1,p_BAR_1,data_26_1,data_lo_1,data_olr_1=load_data(path,lo,False)
+        nnames+=1
+        ###################
+        print '*************************************************'
+        enter=raw_input('Enter another run name?? (y or n)')
+        if 'y' in enter:
+            runname_2,lon_arr_2,lat_arr_2,oom_2,surfp_2,p_BAR_2,data_26_2,data_lo_2,data_olr_2=load_data(path,lo,False)
+            nnames+=1
+            print '*************************************************'
+            ###################
+            enter=raw_input('Enter another run name?? (y or n)')
+            if 'y' in enter:
+                runname_3,lon_arr_3,lat_arr_3,oom_3,surfp_3,p_BAR_3,data_26_3,data_lo_3,data_olr_3=load_data(path,lo,False)
+                nnames+=1
+                print '*************************************************'
+                ###################
+                enter=raw_input('Enter another run name?? (y or n)')
+                if 'y' in enter:
+                    runname_4,lon_arr_4,lat_arr_4,oom_4,surfp_4,p_BAR_4,data_26_4,data_lo_4,data_olr_5=load_data(path,lo,False)
+                    nnames+=1
+                    print '*************************************************'
+                else:
+                    print 'DONE WITH RUNS. ENTERED ',nnames,' RUN NAMES'
+            else:
+                print 'DONE WITH RUNS. ENTERED ',nnames,' RUN NAMES'
+        else:
+            print 'DONE WITH RUNS. ENTERED ',nnames,' RUN NAMES'
+    else:
+        print 'DONE WITH RUNS. ENTERED ',nnames,' RUN NAMES'
+
+    #########################    
+    if plot==0: #TEMPERATURE
+        if lo==True:
+            ind=0
+        else:  
+            ind=5
+    if plot==1:
+        if lo==True:
+            ind=1
+        else:
+            ind=3 # uwind
+    if plot==2:
+        if lo==True:
+            ind=2
+        else:
+            ind=4 # vwind
+    
+    if lo==True:
+        data_1=np.median((np.nanmedian(data_lo_1,axis=4))[lev,:,:,ind],axis=0)
+        if nnames>1:
+            data_2=np.median((np.nanmedian(data_lo_2,axis=4))[lev,:,:,ind],axis=0)
+            if nnames>2:
+                data_3=np.median((np.nanmedian(data_lo_3,axis=4))[lev,:,:,ind],axis=0)
+                if nnames>3:
+                    data_4=np.median((np.nanmedian(data_lo_4,axis=4))[lev,:,:,ind],axis=0)
+    
+    else:
+        data_1=np.median(np.copy(data_26_1[lev,:,:,ind]),axis=0)
+        if nnames>1:
+            data_2=np.median(np.copy(data_26_2[lev,:,:,ind]),axis=0)
+            if nnames>2:
+                data_3=np.median(np.copy(data_26_3[lev,:,:,ind]),axis=0)
+                if nnames>3:
+                    data_4=np.median(np.copy(data_26_4[lev,:,:,ind]),axis=0)
+    
+    
+    plt.figure(figsize=(6.25,10))
+    plt.gcf().subplots_adjust(bottom=0.08,top=0.97,left=0.17,right=0.97)
+    
+    ncolor=40
+    color_list = plt.cm.plasma(np.linspace(0., 1, ncolor))
+    if nnames==1:
+        color1=color_list[int(ncolor/2)]
+    if nnames==2:
+        color1=color_list[int(ncolor/3)]
+        color2=color_list[int(2.*ncolor/3)]
+    if nnames==3:
+        color1=color_list[int(8*ncolor/10)]
+        color2=color_list[int(ncolor/2)]
+        color3=color_list[int(ncolor/10)]
+    if nnames==4:
+        color1=color_list[int(8*ncolor/10)]
+        color2=color_list[int(6*ncolor/10)]
+        color3=color_list[int(3*ncolor/10)]
+        color4=color_list[int(1*ncolor/10)]
+    
+    tn=tn
+    ln=ln
+    plt.plot(data_1,lat_arr_1,linewidth=5.0,linestyle=':',color=color1)
+    plt.figtext(ln,tn,runname_1,fontsize=20,fontproperties=fontb,color=color1)
+    if nnames>1:
+        plt.plot(data_2,lat_arr_2,linewidth=5.0,linestyle='--',color=color2)
+        plt.figtext(ln,tn-0.04,runname_2,fontsize=20,fontproperties=fontb,color=color2)
+        if nnames>2:
+            plt.plot(data_3,lat_arr_3,linewidth=5.0,linestyle='-',color=color3)
+            plt.figtext(ln,tn-0.04*2.,runname_3,fontsize=20,fontproperties=fontb,color=color3)
+            if nnames>3:
+                plt.plot(data_4,lat_arr_4,linewidth=5.0,linestyle='-.',color=color4)
+                plt.figtext(ln,tn-0.04*3.,runname_4,fontsize=20,fontproperties=fontb,color=color4)
+    
+    plt.ylabel('Latitude [degrees]',fontsize=20)
+    if plot==0:
+        plt.xlabel('Temp [K]',fontsize=20)
+    if plot==1:
+        plt.xlabel('E-W Wind [m/s]',fontsize=20)
+    if plot==2:
+        plt.xlabel('N-S Wind [m/s]',fontsize=20)
+    
+    plt.yticks(fontsize=18,fontproperties=font)
+    plt.xticks(fontsize=18,fontproperties=font)
+    
+    if plot==0:
+        plt.savefig(path+runname_1+'/LongAvg_Temps_Comparison.pdf',transparent=True)
+        if nnames>1:
+            plt.savefig(path+runname_2+'/LongAvg_Temps_Comparison.pdf',transparent=True)
+            if nnames>2:
+                plt.savefig(path+runname_3+'/LongAvg_Temps_Comparison.pdf',transparent=True)
+                if nnames>3:
+                    plt.savefig(path+runname_4+'/LongAvg_Temps_Comparison.pdf',transparent=True)
+    if plot==1:
+        plt.savefig(path+runname_1+'/LongAvg_UWinds_Comparison.pdf',transparent=True)
+        if nnames>1:
+            plt.savefig(path+runname_2+'/LongAvg_UWinds_Comparison.pdf',transparent=True)
+            if nnames>2:
+                plt.savefig(path+runname_3+'/LongAvg_UWinds_Comparison.pdf',transparent=True)
+                if nnames>3:
+                    plt.savefig(path+runname_4+'/LongAvg_UWinds_Comparison.pdf',transparent=True)
+    if plot==2:
+        plt.savefig(path+runname_1+'/LongAvg_VWinds_Comparison.pdf',transparent=True)
+        if nnames>1:
+            plt.savefig(path+runname_2+'/LongAvg_VWinds_Comparison.pdf',transparent=True)
+            if nnames>2:
+                plt.savefig(path+runname_3+'/LongAvg_VWinds_Comparison.pdf',transparent=True)
+                if nnames>3:
+                    plt.savefig(path+runname_4+'/LongAvg_VWinds_Comparison.pdf',transparent=True)
+    
+    plt.show()
+    return
+
+
+
