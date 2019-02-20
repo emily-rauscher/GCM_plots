80279a8677432001fc98ef7cee4cf2a6ed10a2e3
erinmmay <mayerin1@gmail.com>
2019-02-20 17:55:40 -0500

    updates to long-avg temps for multiple run comparison

--- igcm_func.py
+++ igcm_func.py
@@ -654,44 +654,35 @@ def lon_avg(plot,path,data,lon_arr,lat_arr,lev,lo,ln,tn,noy):
     plt.show()
     return
 
-def lon_avg_comp(path,plot,lev,lo,ln,tn):
-    nnames=0
-    print '********** Maximum of 4 RUNS currently **********'
-    print '*************************************************'
-    ###################
-    enter='y'#raw_input('Enter another run name?? (y or n)')
-    if 'y' in enter:
-        runname_1,lon_arr_1,lat_arr_1,oom_1,surfp_1,p_BAR_1,data_26_1,data_lo_1,data_olr_1=load_data(path,lo,False)
-        nnames+=1
-        ###################
-        print '*************************************************'
-        enter=raw_input('Enter another run name?? (y or n)')
-        if 'y' in enter:
-            runname_2,lon_arr_2,lat_arr_2,oom_2,surfp_2,p_BAR_2,data_26_2,data_lo_2,data_olr_2=load_data(path,lo,False)
-            nnames+=1
-            print '*************************************************'
-            ###################
-            enter=raw_input('Enter another run name?? (y or n)')
-            if 'y' in enter:
-                runname_3,lon_arr_3,lat_arr_3,oom_3,surfp_3,p_BAR_3,data_26_3,data_lo_3,data_olr_3=load_data(path,lo,False)
-                nnames+=1
-                print '*************************************************'
-                ###################
-                enter=raw_input('Enter another run name?? (y or n)')
-                if 'y' in enter:
-                    runname_4,lon_arr_4,lat_arr_4,oom_4,surfp_4,p_BAR_4,data_26_4,data_lo_4,data_olr_5=load_data(path,lo,False)
-                    nnames+=1
-                    print '*************************************************'
-                else:
-                    print 'DONE WITH RUNS. ENTERED ',nnames,' RUN NAMES'
-            else:
-                print 'DONE WITH RUNS. ENTERED ',nnames,' RUN NAMES'
-        else:
-            print 'DONE WITH RUNS. ENTERED ',nnames,' RUN NAMES'
-    else:
-        print 'DONE WITH RUNS. ENTERED ',nnames,' RUN NAMES'
+def add_run(path,lo,lev,ind,runnum,curdata,lat_arr_out,runname_out):
+    runname,lon_arr,lat_arr,oom,surp,p_BAR,data_26,data_lo,data_olr=load_data(path,lo,False,False)
+    runnum+=1
+    
+    levl=np.argmin(np.abs(p_BAR-lev))
+    print 'closest level to ', lev, ' BARS is #',levl
 
-    #########################    
+    if lo==True:
+        data=np.median((np.nanmedian(data_lo,axis=4))[levl,:,:,ind],axis=0)
+    
+    else:
+        data=np.median(np.copy(data_26[levl,:,:,ind]),axis=0)
+        
+    if runnum==1:
+        curdata=data
+        lat_arr_out=lat_arr
+        runname_out=np.array([runname])
+    else:
+        curdata=np.vstack((curdata,data))
+        lat_arr_out=np.vstack((lat_arr_out,lat_arr))
+        runname_out=np.append(runname_out,runname)
+    print curdata.shape
+    print runname_out
+    return curdata,lat_arr_out,runname_out,runnum
+
+    
+
+def lon_avg_comp(path,plot,lev,lo,ln,tn):
+    
     if plot==0: #TEMPERATURE
         if lo==True:
             ind=0
@@ -707,59 +698,28 @@ def lon_avg_comp(path,plot,lev,lo,ln,tn):
             ind=2
         else:
             ind=4 # vwind
-    
-    if lo==True:
-        data_1=np.median((np.nanmedian(data_lo_1,axis=4))[lev,:,:,ind],axis=0)
-        if nnames>1:
-            data_2=np.median((np.nanmedian(data_lo_2,axis=4))[lev,:,:,ind],axis=0)
-            if nnames>2:
-                data_3=np.median((np.nanmedian(data_lo_3,axis=4))[lev,:,:,ind],axis=0)
-                if nnames>3:
-                    data_4=np.median((np.nanmedian(data_lo_4,axis=4))[lev,:,:,ind],axis=0)
-    
-    else:
-        data_1=np.median(np.copy(data_26_1[lev,:,:,ind]),axis=0)
-        if nnames>1:
-            data_2=np.median(np.copy(data_26_2[lev,:,:,ind]),axis=0)
-            if nnames>2:
-                data_3=np.median(np.copy(data_26_3[lev,:,:,ind]),axis=0)
-                if nnames>3:
-                    data_4=np.median(np.copy(data_26_4[lev,:,:,ind]),axis=0)
-    
+                    
+    curdata=0
+    lat_arr_out=0
+    runname_out=0
+    add_run_in='y'
+    runnum=0
+    while add_run_in=='y':
+        curdata,lat_arr_out,runname_out,runnum=add_run(path,lo,lev,ind,runnum,curdata,lat_arr_out,runname_out)
+        print '--------------'
+        add_run_in=raw_input('Enter another run name?? (y or n)')    
     
     plt.figure(figsize=(6.25,10))
     plt.gcf().subplots_adjust(bottom=0.08,top=0.97,left=0.17,right=0.97)
     
-    ncolor=40
-    color_list = plt.cm.plasma(np.linspace(0., 1, ncolor))
-    if nnames==1:
-        color1=color_list[int(ncolor/2)]
-    if nnames==2:
-        color1=color_list[int(ncolor/3)]
-        color2=color_list[int(2.*ncolor/3)]
-    if nnames==3:
-        color1=color_list[int(8*ncolor/10)]
-        color2=color_list[int(ncolor/2)]
-        color3=color_list[int(ncolor/10)]
-    if nnames==4:
-        color1=color_list[int(8*ncolor/10)]
-        color2=color_list[int(6*ncolor/10)]
-        color3=color_list[int(3*ncolor/10)]
-        color4=color_list[int(1*ncolor/10)]
+    nnames=curdata.shape[0]
+    color_list = plt.cm.plasma(np.linspace(0., 1, nnames+2))
     
     tn=tn
     ln=ln
-    plt.plot(data_1,lat_arr_1,linewidth=5.0,linestyle=':',color=color1)
-    plt.figtext(ln,tn,runname_1,fontsize=20,fontproperties=fontb,color=color1)
-    if nnames>1:
-        plt.plot(data_2,lat_arr_2,linewidth=5.0,linestyle='--',color=color2)
-        plt.figtext(ln,tn-0.04,runname_2,fontsize=20,fontproperties=fontb,color=color2)
-        if nnames>2:
-            plt.plot(data_3,lat_arr_3,linewidth=5.0,linestyle='-',color=color3)
-            plt.figtext(ln,tn-0.04*2.,runname_3,fontsize=20,fontproperties=fontb,color=color3)
-            if nnames>3:
-                plt.plot(data_4,lat_arr_4,linewidth=5.0,linestyle='-.',color=color4)
-                plt.figtext(ln,tn-0.04*3.,runname_4,fontsize=20,fontproperties=fontb,color=color4)
+    for i in range(0,curdata.shape[0]):
+        plt.plot(curdata[i,:],lat_arr_out[i,:],linewidth=5.0,linestyle='--',color=color_list[i+1])
+        plt.figtext(ln,tn-0.04*i,runname_out[i],fontsize=20,fontproperties=fontb,color=color_list[i+1])
     
     plt.ylabel('Latitude [degrees]',fontsize=20)
     if plot==0:
@@ -773,29 +733,15 @@ def lon_avg_comp(path,plot,lev,lo,ln,tn):
     plt.xticks(fontsize=18,fontproperties=font)
     
     if plot==0:
-        plt.savefig(path+runname_1+'/LongAvg_Temps_Comparison.pdf',transparent=True)
-        if nnames>1:
-            plt.savefig(path+runname_2+'/LongAvg_Temps_Comparison.pdf',transparent=True)
-            if nnames>2:
-                plt.savefig(path+runname_3+'/LongAvg_Temps_Comparison.pdf',transparent=True)
-                if nnames>3:
-                    plt.savefig(path+runname_4+'/LongAvg_Temps_Comparison.pdf',transparent=True)
+        for i in range(0,len(runname_out)):
+            plt.savefig(path+str(runname_out[i])+'/LongAvg_Temps_Comparison.pdf',transparent=True)
+
     if plot==1:
-        plt.savefig(path+runname_1+'/LongAvg_UWinds_Comparison.pdf',transparent=True)
-        if nnames>1:
-            plt.savefig(path+runname_2+'/LongAvg_UWinds_Comparison.pdf',transparent=True)
-            if nnames>2:
-                plt.savefig(path+runname_3+'/LongAvg_UWinds_Comparison.pdf',transparent=True)
-                if nnames>3:
-                    plt.savefig(path+runname_4+'/LongAvg_UWinds_Comparison.pdf',transparent=True)
+        plt.savefig(path+'/LongAvg_UWinds_Comparison.pdf',transparent=True)
+
     if plot==2:
-        plt.savefig(path+runname_1+'/LongAvg_VWinds_Comparison.pdf',transparent=True)
-        if nnames>1:
-            plt.savefig(path+runname_2+'/LongAvg_VWinds_Comparison.pdf',transparent=True)
-            if nnames>2:
-                plt.savefig(path+runname_3+'/LongAvg_VWinds_Comparison.pdf',transparent=True)
-                if nnames>3:
-                    plt.savefig(path+runname_4+'/LongAvg_VWinds_Comparison.pdf',transparent=True)
+        plt.savefig(path+'/LongAvg_VWinds_Comparison.pdf',transparent=True)
+
     
     plt.show()
     return
