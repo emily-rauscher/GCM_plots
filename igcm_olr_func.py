OLR has last orbit average and fixed plot naming

igcm_olr_func.py
@@ -15,81 +15,27 @@ fontb.set_weight('bold')
 params = {'font.family': 'serif',}
 matplotlib.rcParams.update(params)
 
-def igcm_olr(path, runname, oom, surfp, radea, createplot, savefig, savenamesw,savenamelw, BOTH):
+def igcm_olr(path, runname, oom, surfp, radea, createplot, savefig, savenamelw,savenamesw, BOTH,LastOrb):
     # wave == 'LW' (1) or "SW' (2)
     # both == total energy outgoing (SW + LW)
     
-        
-    ##### load files #####
-    with open(path+runname+'/fort.64') as f:
-        first_line=f.readline()
-        nlat,nlon,nlev=first_line.split()
-        nlat,nlon,nlev=int(nlat),int(nlon),int(nlev)
-        print '  '
-        print ' ....reading fort.64 (LW)'
-        print '       nlat=', nlat, 'nlon=', nlon, 'nlev=', nlon
-    
-    #if nlat!=len(lat_arr) or nlon!=len(lon_arr):
-    #    print 'ERROR: Lat or Lon mis-match, check files'
-    #    exit()
-        
-    data_64=np.empty([nlat*nlon,3])*0.0
-    l=0
-    with open(path+runname+'/fort.64') as f:
-        for line in f:
-            if l==0:
-                l+=1
-                continue
-            if l>nlat*nlon:
-                continue
-                l+=1
-            #print line, line.split()
-            data_64[l-1] = line.split()
-            l+=1
-        print '       END OF FILE: DONE'
-    f.close()
-    
-    print data_64.shape
-    
-    lon_arr_f=data_64[:,0]
-    lon_arr=np.array([])
-    for l in range(0,len(lon_arr_f)):
-        el=lon_arr_f[l]
-        if not el in lon_arr:
-            lon_arr=np.append(lon_arr,el)
-
-    lat_arr_f=data_64[:,1]
-    lat_arr=np.array([])
-    for l in range(0,len(lat_arr_f)):
-        el=lat_arr_f[l]
-        if not el in lat_arr:
-            lat_arr=np.append(lat_arr,el)
-            
-    data_lw=np.empty([nlon,nlat,3])
-    for l in range(0,data_64.shape[0]):
-        lon,lat=data_64[l,:2]
-        lon_i,lat_i=np.where(lon_arr==lon)[0][0],np.where(lat_arr==lat)[0][0]
-        data_lw[lon_i,lat_i,:]=data_64[l,:]
-            
-    print 'LW', data_lw.shape
-    
-    #######################
-    if BOTH==True:
-        with open(path+runname+'/fort.65') as f:
+    if LastOrb==False:     
+        ##### load files #####
+        with open(path+runname+'/fort.64') as f:
             first_line=f.readline()
             nlat,nlon,nlev=first_line.split()
             nlat,nlon,nlev=int(nlat),int(nlon),int(nlev)
             print '  '
-            print ' ....reading fort.65 (SW)'
+            print ' ....reading fort.64 (LW)'
             print '       nlat=', nlat, 'nlon=', nlon, 'nlev=', nlev
 
         #if nlat!=len(lat_arr) or nlon!=len(lon_arr):
         #    print 'ERROR: Lat or Lon mis-match, check files'
         #    exit()
 
-        data_65=np.empty([nlat*nlon,3])*0.0
+        data_64=np.empty([nlat*nlon,3])*0.0
         l=0
-        with open(path+runname+'/fort.65') as f:
+        with open(path+runname+'/fort.64') as f:
             for line in f:
                 if l==0:
                     l+=1
@@ -98,46 +44,226 @@ def igcm_olr(path, runname, oom, surfp, radea, createplot, savefig, savenamesw,s
                     continue
                     l+=1
                 #print line, line.split()
-                data_65[l-1] = line.split()
+                data_64[l-1] = line.split()
                 l+=1
             print '       END OF FILE: DONE'
         f.close()
 
-        print data_65.shape
+        print data_64.shape
 
-        lon_arr_f=data_65[:,0]
+        lon_arr_f=data_64[:,0]
         lon_arr=np.array([])
-        for l in range(0,len(lon_arr_f)):
-            el=lon_arr_f[l]
-            if not el in lon_arr:
-                lon_arr=np.append(lon_arr,el)
+        l=0
+        while l<data_64.shape[0]:
+            lon_arr=np.append(lon_arr,lon_arr_f[l])
+            l+=nlat
 
-        lat_arr_f=data_65[:,1]
-        lat_arr=np.array([])
-        for l in range(0,len(lat_arr_f)):
-            el=lat_arr_f[l]
-            if not el in lat_arr:
-                lat_arr=np.append(lat_arr,el)
+        lat_arr=data_64[:nlat,1]
 
-        data_sw=np.empty([nlon,nlat,3])
-        for l in range(0,data_65.shape[0]):
-            lon,lat=data_65[l,:2]
+        data_lw=np.empty([nlon,nlat,3])
+        for l in range(0,data_64.shape[0]):
+            lon,lat=data_64[l,:2]
             lon_i,lat_i=np.where(lon_arr==lon)[0][0],np.where(lat_arr==lat)[0][0]
-            data_sw[lon_i,lat_i,:]=data_65[l,:]
+            data_lw[lon_i,lat_i,:]=data_64[l,:]
+            
+        data_lw=data_lw[:,:,2]
 
-        print 'SW', data_sw.shape
+        print 'LW', data_lw.shape
+
+        #######################
+        if BOTH==True:
+            with open(path+runname+'/fort.65') as f:
+                first_line=f.readline()
+                nlat,nlon,nlev=first_line.split()
+                nlat,nlon,nlev=int(nlat),int(nlon),int(nlev)
+                print '  '
+                print ' ....reading fort.65 (SW)'
+                print '       nlat=', nlat, 'nlon=', nlon, 'nlev=', nlev
+
+            #if nlat!=len(lat_arr) or nlon!=len(lon_arr):
+            #    print 'ERROR: Lat or Lon mis-match, check files'
+            #    exit()
+
+            data_65=np.empty([nlat*nlon,3])*0.0
+            l=0
+            with open(path+runname+'/fort.65') as f:
+                for line in f:
+                    if l==0:
+                        l+=1
+                        continue
+                    if l>nlat*nlon:
+                        continue
+                        l+=1
+                    #print line, line.split()
+                    data_65[l-1] = line.split()
+                    l+=1
+                print '       END OF FILE: DONE'
+            f.close()
+
+            print data_65.shape
+
+            lon_arr_f=data_65[:,0]
+            lon_arr=np.array([])
+            l=0
+            while l<data_65.shape[0]:
+                lon_arr=np.append(lon_arr,lon_arr_f[l])
+                l+=nlat
+
+            lat_arr=data_65[:nlat,1]
+
+            data_sw=np.empty([nlon,nlat,3])
+            for l in range(0,data_65.shape[0]):
+                lon,lat=data_65[l,:2]
+                lon_i,lat_i=np.where(lon_arr==lon)[0][0],np.where(lat_arr==lat)[0][0]
+                data_sw[lon_i,lat_i,:]=data_65[l,:]
+            
+            data_sw=data_sw[:,:,2]
+            print 'SW', data_sw.shape
+            
+    if LastOrb==True:
+        print ' DOING LAST ORBIT AVERAGES....'
+        filen=6400
+        while filen<6490:
+            file_lw='fort.'+str(int(filen))
+            if filen==6400:
+                with open(path+runname+'/'+file_lw) as f:
+                    first_line=f.readline()
+                    nlat,nlon,nlev=first_line.split()
+                    nlat,nlon,nlev=int(nlat),int(nlon),int(nlev)
+                    data_64=np.empty([nlat*nlon])*0.0
+                    print '  '
+                    print ' ....reading fort.6400 (LW)'
+                    print '       nlat=', nlat, 'nlon=', nlon, 'nlev=', nlev
+                    print '  '
+            if filen>6400:
+                print ' ....reading fort.',filen,' (LW)'
+
+            l=0
+            data_64h=np.empty([nlat*nlon,3])*0.0
+            with open(path+runname+'/'+file_lw) as f:
+                for line in f:
+                    if l==0:
+                        l+=1
+                        continue
+                    if l>nlat*nlon:
+                        continue
+                        l+=1
+                    #print line, line.split()
+                    data_64h[l-1] = line.split()
+                    l+=1
+                print '       END OF FILE: DONE'
+            f.close()
+            
+            lon_arr_f=data_64h[:,0]
+            lon_arr=np.array([])
+            l=0
+            while l<data_64h.shape[0]:
+                lon_arr=np.append(lon_arr,lon_arr_f[l])
+                l+=nlat
+
+            lat_arr=data_64h[:nlat,1]
+
+            data_lwh=np.empty([nlon,nlat,3])
+
+            for l in range(0,data_64h.shape[0]):
+                lon,lat=data_64h[l,:2]
+                #print l,lon,lat
+                lon_i,lat_i=np.where(lon_arr==lon)[0][0],np.where(lat_arr==lat)[0][0]
+                #print l,lon,lat,lon_i,lat_i
+
+                data_lwh[lon_i,lat_i,:]=data_64h[l,:]
+            
+            data_lwh=data_lwh[:,:,2]
+            
+            if filen==6400:
+                data_lw=np.copy(data_lwh)
+            elif filen>6400:
+                data_lw=np.dstack((data_lw,data_lwh))
+            filen+=1
+                             
+
+            #print '            (', data_64.shape, ' )'
+        print data_lw.shape
+        data_lw=np.nanmean(data_lw,axis=2)
+        print data_lw.shape
+
+        print 'LW', data_lw.shape
+
+        #######################
+        if BOTH==True:
+            filen=6500
+            while filen<6590:
+                file_sw='fort.'+str(int(filen))
+                if filen==6500:
+                    with open(path+runname+'/'+file_sw) as f:
+                        first_line=f.readline()
+                        nlat,nlon,nlev=first_line.split()
+                        nlat,nlon,nlev=int(nlat),int(nlon),int(nlev)
+                        data_65=np.empty([nlat*nlon,3])*0.0
+                        print '  '
+                        print ' ....reading fort.6500 (SW)'
+                        print '       nlat=', nlat, 'nlon=', nlon, 'nlev=', nlon
+                        print '  '
+                if filen>6500:
+                    print  ' ....reading fort.',filen,' (SW)'
+
+                l=0
+                data_65h=np.empty([nlat*nlon,3])*0.0
+                with open(path+runname+'/'+file_sw) as f:
+                    for line in f:
+                        if l==0:
+                            l+=1
+                            continue
+                        if l>nlat*nlon:
+                            continue
+                            l+=1
+                        #print line, line.split()
+                        data_65h[l-1] = line.split()
+                        l+=1
+                    print '       END OF FILE: DONE'
+                f.close()
+                
+                lon_arr_f=data_65h[:,0]
+                lon_arr=np.array([])
+                l=0
+                while l<data_65h.shape[0]:
+                    lon_arr=np.append(lon_arr,lon_arr_f[l])
+                    l+=nlat
+
+                lat_arr=data_65h[:nlat,1]
+
+                data_swh=np.empty([nlon,nlat,3])
+                for l in range(0,data_65h.shape[0]):
+                    lon,lat=data_65h[l,:2]
+                    lon_i,lat_i=np.where(lon_arr==lon)[0][0],np.where(lat_arr==lat)[0][0]
+                    data_swh[lon_i,lat_i,:]=data_65h[l,:]
+                
+                data_swh=data_swh[:,:,2]
+               
+                if filen==6500:
+                    data_sw=np.copy(data_swh)
+                elif filen>6500:
+                    data_sw=np.dstack((data_sw,data_swh))
+                filen+=1
+
+            print data_sw.shape
+            data_sw=np.nanmean(data_sw,axis=2)
+            print data_sw.shape
+            
+            print 'SW', data_sw.shape
+
     
     #######################
     # Sum values for total outgoing radiation (taken from igcm_olr.pro)
     # ' Total integrated output (W): ',total(olr*cos(lat*!pi/180.))*(2.*!pi/nlon)*(!pi/nlat)*radea^2
-    total_lw=np.nansum(data_lw[:,:,2]*np.cos(lat_arr*np.pi/180.))*(2*np.pi/nlon)*(np.pi/nlat)*radea**2.
-    dayside_lw=((np.nansum(data_lw[0:np.int(nlon/4),:,2]*np.cos(lat_arr*np.pi/180.))  
-                                      +np.nansum(data_lw[np.int(nlon*3./4):nlon-1,:,2]*np.cos(lat_arr*np.pi/180.))) 
+    total_lw=np.nansum(data_lw[:,:]*np.cos(lat_arr*np.pi/180.))*(2*np.pi/nlon)*(np.pi/nlat)*radea**2.
+    dayside_lw=((np.nansum(data_lw[0:np.int(nlon/4),:]*np.cos(lat_arr*np.pi/180.))  
+                                      +np.nansum(data_lw[np.int(nlon*3./4):nlon-1,:]*np.cos(lat_arr*np.pi/180.))) 
                                     *(2.*np.pi/nlon)*(np.pi/nlat)*radea**2)
     if BOTH==True:
-        total_sw=np.nansum(data_sw[:,:,2]*np.cos(lat_arr*np.pi/180.))*(2*np.pi/nlon)*(np.pi/nlat)*radea**2.
-        dayside_sw=((np.nansum(data_sw[0:np.int(nlon/4),:,2]*np.cos(lat_arr*np.pi/180.)) 
-                                      +np.nansum(data_sw[np.int(nlon*3./4):nlon-1,:,2]*np.cos(lat_arr*np.pi/180.))) 
+        total_sw=np.nansum(data_sw[:,:]*np.cos(lat_arr*np.pi/180.))*(2*np.pi/nlon)*(np.pi/nlat)*radea**2.
+        dayside_sw=((np.nansum(data_sw[0:np.int(nlon/4),:]*np.cos(lat_arr*np.pi/180.)) 
+                                      +np.nansum(data_sw[np.int(nlon*3./4):nlon-1,:]*np.cos(lat_arr*np.pi/180.))) 
                                     *(2.*np.pi/nlon)*(np.pi/nlat)*radea**2)
     print '******************************'
     print 'Total Integrated Output (W):'
@@ -161,9 +287,9 @@ def igcm_olr(path, runname, oom, surfp, radea, createplot, savefig, savenamesw,s
         CENTER=np.zeros([nlon,nlat])
         for i in range(0,len(lon_arr)):
             if i<nlon/2:
-                CENTER[i+nlon/2,:]=data_lw[i,:,2]
+                CENTER[i+nlon/2,:]=data_lw[i,:]
             if i>=nlon/2:
-                CENTER[i-nlon/2,:]=data_lw[i,:,2]
+                CENTER[i-nlon/2,:]=data_lw[i,:]
         plt_data=CENTER
         plt_lon=np.linspace(-180,180,nlon)
         
@@ -196,9 +322,9 @@ def igcm_olr(path, runname, oom, surfp, radea, createplot, savefig, savenamesw,s
             CENTER=np.zeros([nlon,nlat])
             for i in range(0,len(lon_arr)):
                 if i<nlon/2:
-                    CENTER[i+nlon/2,:]=data_sw[i,:,2]
+                    CENTER[i+nlon/2,:]=data_sw[i,:]
                 if i>=nlon/2:
-                    CENTER[i-nlon/2,:]=data_sw[i,:,2]
+                    CENTER[i-nlon/2,:]=data_sw[i,:]
             plt_data=CENTER
             plt_lon=np.linspace(-180,180,nlon)
 
@@ -225,6 +351,6 @@ def igcm_olr(path, runname, oom, surfp, radea, createplot, savefig, savenamesw,s
                 plt.savefig(savenamesw,rasterized=True,transparent=True)
             plt.show()
     if BOTH==True:
-        return data_lw, data_sw, total_lw, total_sw
+        return data_lw, data_sw, total_lw, total_sw,lon_arr,lat_arr
     else:
-        return data_lw,np.nan, total_lw,np.nan
+        return data_lw,np.nan, total_lw,np.nan,lon_arr,lat_arr